322adccd853eaa689462400a7915e62de12ef260
Maithilee <muleymaithilee2411@gmail.com>
2023-12-29 15:57:01 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  [x: string]: any;
+  form: FormGroup = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+  
+   
+    acceptTerms: new FormControl(false),
+  });
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+       
+        title: ['', [Validators.required, Validators.email]],
+        description: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+          ]
+        ],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue]
+      },
+      
+    );
+  }
+
+  
+}
---
+++ src/app/models/tutorial .spec.ts
@@ -0,0 +1,7 @@
+import { Tutorial } from './tutorial ';
+
+describe('Tutorial', () => {
+  it('should create an instance', () => {
+    expect(new Tutorial()).toBeTruthy();
+  });
+});
